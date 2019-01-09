f1cb916271f7515751c5f319e5a0718b7ecabdaa
Mary West <westmarye1219@gmail.com>
2019-01-08 20:46:04 -0600

    added icons

--- src/components/MyNavbar/MyNavbar.js
+++ src/components/MyNavbar/MyNavbar.js
@@ -35,19 +35,19 @@ class MyNavbar extends React.Component {
         return (
           <Nav className='ml-auto' navbar>
             <NavItem>
-              <NavLink tag={RRNavLink} to='/friends'>Friends</NavLink>
+              <NavLink tag={RRNavLink} to='/friends'><i class="fas fa-user-friends fa-2x"></i></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={RRNavLink} to='/messages'>Messages</NavLink>
+              <NavLink tag={RRNavLink} to='/messages'><i class="fas fa-comments fa-2x"></i></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={RRNavLink} to='/weather'>Weather</NavLink>
+              <NavLink tag={RRNavLink} to='/weather'><i class="fas fa-bolt fa-2x"></i></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={RRNavLink} to='/events'>Events</NavLink>
+              <NavLink tag={RRNavLink} to='/events'><i class="fas fa-calendar-week fa-2x"></i></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={RRNavLink} to='/articles'>Articles</NavLink>
+              <NavLink tag={RRNavLink} to='/articles'><i class="fas fa-newspaper fa-2x"></i></NavLink>
             </NavItem>
             <NavItem>
               <NavLink onClick={logoutClickEvent}>Logout</NavLink>
